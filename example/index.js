37fc1c6368492719a81e863f04656804cebdefb4
sarah <sarahewever@gmail.com>
2019-06-25 22:08:02 -0400

    match styles

--- example/index.js
+++ example/index.js
@@ -1,28 +1,29 @@
-'use strict'
+"use strict";
 
-const emojiUnicode = require('../lib'),
-  toEmoji = require('emoji-name-map')
+const emojiUnicode = require("../lib")
+    , toEmoji = require("emoji-name-map")
+    ;
 
-console.log(emojiUnicode('📻'))
+console.log(emojiUnicode("📻"));
 // => 1f4fb
 
-console.log(emojiUnicode('🏳️‍🌈'))
-// => 1F3F3 FE0F 200D 1F308
-
-console.log(emojiUnicode('👩🏽‍💻'))
-// => 1F469 1F3FD 200D 1F4BB
-
-console.log(emojiUnicode.raw('📻'))
+console.log(emojiUnicode.raw("📻"));
 // => 128251
 
-console.log(emojiUnicode.raw('🏳️‍🌈'))
-// => 127987 65039 8205 127752
+console.log(emojiUnicode("👩🏽‍💻"));
+// => 1F469 1F3FD 200D 1F4BB
 
-console.log(emojiUnicode.raw('👩🏽‍💻'))
+console.log(emojiUnicode.raw("👩🏽‍💻"));
 // => 128105 127997 8205 128187
 
-console.log(emojiUnicode(toEmoji.get(':radio:')))
+console.log(emojiUnicode.raw("🏳️‍🌈"));
+// => 127987 65039 8205 127752
+
+console.log(emojiUnicode("🏳️‍🌈"));
+// => 1F3F3 FE0F 200D 1F308
+
+console.log(emojiUnicode(toEmoji.get(":radio:")));
 // => 1f4fb
 
-console.log(emojiUnicode(toEmoji.get('radio')))
+console.log(emojiUnicode(toEmoji.get("radio")));
 // => 1f4fb
--- test/index.js
+++ test/index.js
@@ -1,13 +1,16 @@
-'use strict'
+"use strict";
 
-const tester = require('tester'),
-  toUnicode = require('..')
-tester.describe('✨', t => {
-  t.should('get unicode codes', () => {
-    t.expect(toUnicode('📻')).toEqual('1f4fb')
-    t.expect(toUnicode('🍦')).toEqual('1f366')
-    t.expect(toUnicode('🍨')).toEqual('1f368')
-    t.expect(toUnicode('💅🏿')).toEqual('1f485 1f3ff')
-    t.expect(toUnicode('👯‍♂️')).toEqual('1f46f 200d 2642 fe0f')
-  })
-})
+const tester = require("tester")
+    , toUnicode = require("..")
+    ;
+
+tester.describe("✨", t => {
+    t.should("get unicode codes", () => {
+        t.expect(toUnicode("📻")).toEqual("1f4fb");
+        t.expect(toUnicode("🍦")).toEqual("1f366");
+        t.expect(toUnicode("🍨")).toEqual("1f368");
+        t.expect(toUnicode("💅🏿")).toEqual("1f485 1f3ff");
+    	t.expect(toUnicode("👯‍♂️")).toEqual("1f46f 200d 2642 fe0f");
+    	t.expect(toUnicode("🇪🇨")).toEqual("1f1ea 1f1e8");
+    });
+});
